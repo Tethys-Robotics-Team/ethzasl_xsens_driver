4d735fdecd6c60c15e68e27e2d346398de02ee4a
Johannes Lienhart <jolienhart8@gmail.com>
2022-07-11 11:31:41 +0200

    adjustments for rosnetwork with namespaces and individual nodenames

--- nodes/mtnode.py
+++ nodes/mtnode.py
@@ -730,7 +730,7 @@ class XSensDriver(object):
         if self.pub_imu:
             self.imu_msg.header = self.h
             if self.imu_pub is None:
-                self.imu_pub = rospy.Publisher('imu/data', Imu, queue_size=10)
+                self.imu_pub = rospy.Publisher('data', Imu, queue_size=10)
             self.imu_pub.publish(self.imu_msg)
         if self.pub_raw_gps:
             self.raw_gps_msg.header = self.h
@@ -751,7 +751,7 @@ class XSensDriver(object):
         if self.pub_mag:
             self.mag_msg.header = self.h
             if self.mag_pub is None:
-                self.mag_pub = rospy.Publisher('imu/mag', MagneticField,
+                self.mag_pub = rospy.Publisher('mag', MagneticField,
                                                queue_size=10)
             self.mag_pub.publish(self.mag_msg)
         if self.pub_temp:
