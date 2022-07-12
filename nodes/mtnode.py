0037c78f3f6b2a82be5929b029c79089eb6afce6
Johannes Lienhart <jolienhart8@gmail.com>
2022-07-12 16:30:32 +0200

    adapted all topics to work with ns

--- nodes/mtnode.py
+++ nodes/mtnode.py
@@ -129,7 +129,7 @@ class XSensDriver(object):
         self.old_bGPS = 256  # publish GPS only if new
 
         # publish a string version of all data; to be parsed by clients
-        self.str_pub = rospy.Publisher('imu/imu_data_str', String, queue_size=10)
+        self.str_pub = rospy.Publisher('imu_data_str', String, queue_size=10)
         self.last_delta_q_time = None
         self.delta_q_rate = None
 
@@ -235,7 +235,7 @@ class XSensDriver(object):
             # refs could be published simultaneously
             if self.time_ref_pub is None:
                 self.time_ref_pub = rospy.Publisher(
-                    'imu/time_reference', TimeReference, queue_size=10)
+                    'time_reference', TimeReference, queue_size=10)
             time_ref_msg = TimeReference()
             time_ref_msg.header = self.h
             time_ref_msg.time_ref.secs = secs
@@ -735,17 +735,17 @@ class XSensDriver(object):
         if self.pub_raw_gps:
             self.raw_gps_msg.header = self.h
             if self.raw_gps_pub is None:
-                self.raw_gps_pub = rospy.Publisher('imu/raw_fix', NavSatFix, queue_size=10)
+                self.raw_gps_pub = rospy.Publisher('raw_fix', NavSatFix, queue_size=10)
             self.raw_gps_pub.publish(self.raw_gps_msg)
         if self.pub_pos_gps:
             self.pos_gps_msg.header = self.h
             if self.pos_gps_pub is None:
-                self.pos_gps_pub = rospy.Publisher('imu/fix', NavSatFix, queue_size=10)
+                self.pos_gps_pub = rospy.Publisher('fix', NavSatFix, queue_size=10)
             self.pos_gps_pub.publish(self.pos_gps_msg)
         if self.pub_vel:
             self.vel_msg.header = self.h
             if self.vel_pub is None:
-                self.vel_pub = rospy.Publisher('imu/velocity', TwistStamped,
+                self.vel_pub = rospy.Publisher('velocity', TwistStamped,
                                                queue_size=10)
             self.vel_pub.publish(self.vel_msg)
         if self.pub_mag:
@@ -757,35 +757,35 @@ class XSensDriver(object):
         if self.pub_temp:
             self.temp_msg.header = self.h
             if self.temp_pub is None:
-                self.temp_pub = rospy.Publisher('imu/temperature', Temperature,
+                self.temp_pub = rospy.Publisher('temperature', Temperature,
                                                 queue_size=10)
             self.temp_pub.publish(self.temp_msg)
         if self.pub_press:
             self.press_msg.header = self.h
             if self.press_pub is None:
-                self.press_pub = rospy.Publisher('imu/pressure', FluidPressure,
+                self.press_pub = rospy.Publisher('pressure', FluidPressure,
                                                  queue_size=10)
             self.press_pub.publish(self.press_msg)
         if self.pub_anin1:
             if self.analog_in1_pub is None:
-                self.analog_in1_pub = rospy.Publisher('imu/analog_in1',
+                self.analog_in1_pub = rospy.Publisher('analog_in1',
                                                       UInt16, queue_size=10)
             self.analog_in1_pub.publish(self.anin1_msg)
         if self.pub_anin2:
             if self.analog_in2_pub is None:
-                self.analog_in2_pub = rospy.Publisher('imu/analog_in2', UInt16,
+                self.analog_in2_pub = rospy.Publisher('analog_in2', UInt16,
                                                       queue_size=10)
             self.analog_in2_pub.publish(self.anin2_msg)
         if self.pub_ecef:
             self.ecef_msg.header = self.h
             if self.ecef_pub is None:
-                self.ecef_pub = rospy.Publisher('imu/ecef', PointStamped,
+                self.ecef_pub = rospy.Publisher('ecef', PointStamped,
                                                 queue_size=10)
             self.ecef_pub.publish(self.ecef_msg)
         if self.pub_diag:
             self.diag_msg.header = self.h
             if self.diag_pub is None:
-                self.diag_pub = rospy.Publisher('imu/diagnostics',
+                self.diag_pub = rospy.Publisher('diagnostics',
                                                 DiagnosticArray, queue_size=10)
             self.diag_pub.publish(self.diag_msg)
         # publish string representation
